157cc6f0b0e88d3eb58fcb1b548fb90b9c38ab00
Dekyi Tenzin <dekyitenzin@dekyis-air.myfiosgateway.com>
2023-10-17 10:47:21 -0400

    fixed problem 2

--- functions_practice.py
+++ functions_practice.py
@@ -1,9 +1,18 @@
+#problem 1
 def hello():
     print("Hello")
 
-def pack(a, b, c):
-    return pack[a,b,c]
+result = hello()
+print(result)
 
+#problem 2
+def pack(a, b, c):
+    return [a,b,c]
+
+result = pack(1,2,3)
+print(result)
+
+#problem 3
 def eat_lunch(foods):
     if len(foods)== 0:
         print("My lunchbox is empty!")
@@ -13,10 +22,6 @@ def eat_lunch(foods):
                 print(f"First I eat {foods[0]}")
             else:
                 print(f"Next I eat {foods[1]}")
-    
-hello()
-print(pack("a","b","c"))
-print(pack(1,2,3))
-eat_lunch([])
-eat_lunch(["sandwich"])
-eat_lunch(["apple","banana","sandwich","cookie"])
+
+result = eat_lunch(["apple","banana","sandwich","cookie"])
+print(result)
